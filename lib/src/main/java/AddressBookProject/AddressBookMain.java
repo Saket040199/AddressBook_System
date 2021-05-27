cce4c28e1468cedb5bfe4b331b4236bab01d3d29
saket <saketj420@gmail.com>
2021-05-27 10:46:45 +0530

    Adding: Delete contact

--- lib/src/main/java/AddressBookProject/AddressBookMain.java
+++ lib/src/main/java/AddressBookProject/AddressBookMain.java
@@ -21,7 +21,8 @@ public class AddressBookMain {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 : Add contact");
         System.out.println("Enter 2 : Edit contact ");
-        System.out.println("Enter 3 : Show Contact");
+        System.out.println("Enter 3 : Delete Contact");
+        System.out.println("Enter 4 : Show Contact");
         int choose = sc.nextInt();
         switch(choose) {
             case 1 :
@@ -31,8 +32,12 @@ public class AddressBookMain {
             case 2:
                 contactDirectory.editContact();
                 break;
-
+                
             case 3:
+                contactDirectory.deleteContact();
+                break;
+
+            case 4:
                 contactDirectory.show();
                 break;
 
