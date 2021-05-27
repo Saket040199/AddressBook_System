a70f4dd77d172a59da723436b6a70cf4300fbed2
saket <saketj420@gmail.com>
2021-05-27 10:40:45 +0530

    Adding: Edit Contact

--- lib/src/main/java/AddressBookProject/AddressBook.java
+++ lib/src/main/java/AddressBookProject/AddressBook.java
@@ -10,6 +10,22 @@ public class AddressBook {
     private long phoneNo;
     private String email;
     
+    
+
+	public AddressBook(String firstName, String lastName, String address, 
+			           String city, String state, long phoneNo,
+			           String email, int zip) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
 
 	public String getFirstName() {
 		return firstName;
